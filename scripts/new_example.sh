74b6434b95535dcb8a5a1a604e0ee3fa190d25b4
louiscklaw <louiscklaw@gmail.com>
2020-05-21 02:05:05 +0800

    update adding yarn,

--- scripts/new_example.sh
+++ scripts/new_example.sh
@@ -9,4 +9,6 @@ yarn add prettier --dev --exact
 # layout
 yarn add gatsby-plugin-typography react-typography typography typography-theme-fairy-gates
 
+yarn add gatsby-plugin-typography typography react-typography typography-theme-kirkham gatsby-plugin-emotion @emotion/core
+
 gatsby develop
