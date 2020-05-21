8c52cb97b0fcc3e01638ff5dc6314ca27b8b4df4
louiscklaw <louiscklaw@gmail.com>
2020-05-21 17:03:53 +0800

    update buld scripts,

---
+++ scripts/gh_pages.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -ex
+
+rm -rf ../docs
+
+cp -r public ../docs
+
+# done
--- scripts/new_example.sh
+++ scripts/new_example.sh
@@ -22,4 +22,22 @@ yarn add gatsby-plugin-manifest gatsby-plugin-offline
 # page metadata
 yarn add gatsby-plugin-react-helmet react-helmet
 
-gatsby develop
+yarn add @material-ui/core
+
+yarn add @fortawesome/fontawesome-svg-core  @fortawesome/free-brands-svg-icons @fortawesome/react-fontawesome
+# yarn add @fortawesome/free-regular-svg-icons @fortawesome/free-solid-svg-icons
+
+yarn add gatsby-source-filesystem
+
+# gatsby-plugin-google-analytics, UA-167147273-1
+yarn add gatsby-plugin-google-analytics
+
+yarn add gatsby-plugin-sitemap
+
+read -p 'press any key to build'
+
+gatsby build
+
+cp .cache/default-html.js src/html.js
+
+find src |entr -c -s "gatsby develop"
