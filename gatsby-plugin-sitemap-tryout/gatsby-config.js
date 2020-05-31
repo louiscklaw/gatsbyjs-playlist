f31835a0ce0f454f2621ea9067ba66493edf475e
louiscklaw <louiscklaw@gmail.com>
2020-05-31 16:14:48 +0800

    working sitemap plugin,

--- gatsby-plugin-sitemap-tryout/gatsby-config.js
+++ gatsby-plugin-sitemap-tryout/gatsby-config.js
@@ -7,6 +7,43 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        // output: `/some-other-sitemap.xml`,
+        // Exclude specific pages or groups of pages using glob parameters
+        // See: https://github.com/isaacs/minimatch
+        // The example below will exclude the single `path/to/page` and all routes beginning with `category`
+        exclude: [`/category/*`, `/path/to/page`],
+        query: `
+          {
+            site {
+              siteMetadata {
+                siteUrl
+              }
+            }
+            allSitePage {
+              nodes {
+                path
+              }
+            }
+        }`,
+        // resolveSiteUrl: ({site, allSitePage}) => {
+        //   //Alternativly, you may also pass in an environment variable (or any location) at the beginning of your `gatsby-config.js`.
+        //   return site.siteMetadata.siteUrl
+        // },
+        serialize: ({ site, allSitePage }) =>
+          allSitePage.nodes.map(node => {
+            return {
+              url: `${site.siteMetadata.siteUrl}${node.path}`,
+              changefreq: `daily`,
+              priority: 0.7,
+            }
+          })
+      }
+    },
+
+
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
