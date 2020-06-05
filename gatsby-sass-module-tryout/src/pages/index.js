cf53ec4e444f323eed694bfbc7808d46151488c3
louiscklaw <louiscklaw@gmail.com>
2020-06-05 21:01:13 +0800

    ci: init new example

---
+++ gatsby-sass-module-tryout/src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import test_style from './styles.module.scss'
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <div class="testClass">
+      helloworld
+    </div>
+  </Layout>
+)
+
+export default IndexPage
