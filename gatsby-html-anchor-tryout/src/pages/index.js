237239acbb82467899b6fb082a2dfdb7c8fe828e
louiscklaw <louiscklaw@gmail.com>
2020-06-06 15:45:19 +0800

    update test,

--- gatsby-html-anchor-tryout/src/pages/index.js
+++ gatsby-html-anchor-tryout/src/pages/index.js
@@ -7,6 +7,12 @@ import TestParagraph from '../components/test_paragraph'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <a href="#paragraph6">paragraph6</a>
+    <a href="#paragraph5">paragraph5</a>
+    <a href="#paragraph4">paragraph4</a>
+    <a href="#paragraph3">paragraph3</a>
+    <a href="#paragraph2">paragraph2</a>
+
     <TestParagraph test_id="paragraph1"></TestParagraph>
     <TestParagraph test_id="paragraph2"></TestParagraph>
     <TestParagraph test_id="paragraph3"></TestParagraph>
