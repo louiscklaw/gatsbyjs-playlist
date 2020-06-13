0e5bf9e3eec4b8acd0a7ec7ff96197ea7906d072
louiscklaw <louiscklaw@gmail.com>
2020-06-14 02:32:33 +0800

    update for check radio,

--- gatsby-bulma-tryout/src/pages/index.js
+++ gatsby-bulma-tryout/src/pages/index.js
@@ -4,12 +4,28 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import BulmaButton from '../components/bulma-button'
+import BulmaSwitch from '../components/bulma-switch'
+import BumlaTagsInput from '../components/bulma-tags-input'
+import BulmaCheckRadio from '../components/bulma-check-radio'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div> helloworld </div>
+
+    <h3>button tryout, modal box tryout</h3>
     <BulmaButton />
+
+    <h3>switch tryout</h3>
+    <BulmaSwitch />
+
+    <h3>tags input, not working</h3>
+    <BumlaTagsInput />
+
+    <h3>check radio</h3>
+    <BulmaCheckRadio />
+
+
   </Layout>
 )
 
