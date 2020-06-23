6ca24fcfca6e68065172ab3019e412f6d9edb500
louiscklaw <louiscklaw@gmail.com>
2020-06-23 22:03:36 +0800

    update working grid with youtube,

--- gatsby-react-grid-layout-tryout/src/pages/index.js
+++ gatsby-react-grid-layout-tryout/src/pages/index.js
@@ -7,22 +7,121 @@ import 'react-resizable/css/styles.css'
 
 import Layout from "../components/layout"
 
-class MyFirstGrid extends React.Component {
-  render() {
-    // layout is an array of objects, see the demo for more complete usage
-    const layout = [
-      {i: 'a', x: 0, y: 0, w: 1, h: 2, static: true},
-      {i: 'b', x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4},
-      {i: 'c', x: 4, y: 0, w: 1, h: 2}
-    ];
-    return (
-      <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
-        <div key="a">a</div>
-        <div key="b">b</div>
-        <div key="c">c</div>
-      </GridLayout>
-    )
+import "./style.css"
+
+function MyFirstGrid(){
+
+  let [layout, setLayout] = React.useState(
+    [
+      {i: 'a', x: 0, y: 0, w: 5, h: 6},
+      {i: 'b', x: 0, y: 0, w: 5, h: 6},
+      {i: 'c', x: 0, y: 0, w: 5, h: 6}
+    ]
+  )
+
+  const handleTestOnClick = () => {
+    setLayout([
+      {i: 'a', x: 0, y: 0, w: 4, h: 5},
+      {i: 'b', x: 0, y: 0, w: 4, h: 5},
+      {i: 'c', x: 0, y: 0, w: 4, h: 5}
+    ])
   }
+
+  const handleMaxOnClick = (e) => {
+    let ele_div = e.target.parentElement
+    let div_key = ele_div.dataset.key
+    setLayout([
+      ...layout.filter( ele => ele.i != div_key),
+      ...layout.filter( ele => ele.i == div_key)
+        .map( t => {
+          return {i: t.i, x: t.x, y: t.y, w: 5, h: 6}
+        })
+      ,
+    ])
+  }
+
+  const handleMinOnClick = (e) => {
+    let ele_div = e.target.parentElement
+    let div_key = ele_div.dataset.key
+    setLayout([
+      ...layout.filter( ele => ele.i != div_key),
+      ...layout.filter( ele => ele.i == div_key)
+        .map( t => {
+          return {i: t.i, x: t.x, y: t.y, w: 4, h: 5}
+        })
+      ,
+    ])
+  }
+
+  React.useEffect(()=>{
+    console.log(layout)
+  },[layout])
+
+  return (
+    <>
+      <button onClick={handleTestOnClick}>Test</button>
+      <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+        <div key="a" className="boxa" data-key="a">
+
+        <div className="handler-max" onClick={handleMaxOnClick}>
+            max
+          </div>
+
+          <div className="handler-min" onClick={handleMinOnClick}>
+            min
+          </div>
+
+          <div className="handler-drag">
+            drag
+          </div>
+        </div>
+
+        <div key="b" className="boxb" data-key="b">
+
+          <iframe
+            src="//www.youtube.com/embed/yCOY82UdFrw"
+            frameborder="0"
+            allowfullscreen
+            class="video"
+            />
+
+          <div className="handler-max" onClick={handleMaxOnClick}>
+            max
+          </div>
+
+          <div className="handler-min" onClick={handleMinOnClick}>
+            min
+          </div>
+
+          <div className="handler-drag">
+            drag
+          </div>
+
+        </div>
+
+        <div key="c" className="boxc" data-key="c">
+
+          <div className="handler-max" onClick={handleMaxOnClick}>
+            max
+          </div>
+
+          <div className="handler-min" onClick={handleMinOnClick}>
+            min
+          </div>
+
+          <div className="handler-drag">
+            drag
+          </div>
+
+        </div>
+
+      </GridLayout>
+      <pre>
+        {JSON.stringify(layout)}
+      </pre>
+    </>
+
+  )
 }
 
 const IndexPage = () => (
