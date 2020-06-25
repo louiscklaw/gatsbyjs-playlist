04d8cf56ba44cdfad2c335f21b6e232742e6e357
louiscklaw <louiscklaw@gmail.com>
2020-06-26 01:45:49 +0800

    adding react-toastify tryout,

---
+++ gatsby-react-toastify-tryout/public/component---src-pages-page-2-js-8882c48010df15553deb.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{EbHT:function(n,e,t){"use strict";t.r(e);var a=t("q1tI"),c=t.n(a),s=t("Wbzz"),i=t("NEmX"),o=t.n(i);e.default=function(){return c.a.createElement(c.a.Fragment,null,"this is page2",c.a.createElement(s.Link,{className:o.a.button,to:"/"},"back"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-8882c48010df15553deb.js.map
---
+++ gatsby-react-toastify-tryout/src/components/bulma-tags-input.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import style from '../scss/desktop.module.scss'
+
+function BumlaTagsInput(){
+  return(
+    <>
+      <input class="input" type="tags" placeholder="Add Tag" value="Tag1,Tag2,Tag3"></input>
+    </>
+  )
+}
+
+export default BumlaTagsInput
