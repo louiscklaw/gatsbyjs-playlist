bfb2109697da3ff631a5c27a865eab76e755af6c
louiscklaw <louiscklaw@gmail.com>
2020-07-03 20:18:18 +0800

    tryout: basically work,

--- gatsby-bulma-modal-tryout/src/pages/index.js
+++ gatsby-bulma-modal-tryout/src/pages/index.js
@@ -6,11 +6,44 @@ import SEO from "../components/seo"
 
 import style from '../scss/style.module.scss'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1 className={style.title}>helloworld</h1>
-  </Layout>
-)
+function IndexPage() {
+  let modal_ref = React.useRef()
+
+  const openModal = () => {
+    modal_ref.current.classList.add(style.isActive)
+  }
+
+  const closeModal = ()=>{
+    modal_ref.current.classList.remove(style.isActive)
+  }
+
+  return(
+    <Layout>
+      <SEO title="Home" />
+
+      <div className={style.modal} ref={modal_ref}>
+        <div className={style.modalBackground}></div>
+        <div className={style.modalCard}>
+          <header className={style.modalCardHead}>
+            <p className={style.modalCardTitle}>Modal title</p>
+            <button className={style.delete} aria-label="close"></button>
+          </header>
+          <section className={style.modalCardBody}>
+            Content
+          </section>
+          <footer className={style.modalCardFoot}>
+            <button className={style.button+' '+style.isSuccess}>Save changes</button>
+            <button className={style.button} onClick={closeModal}>Cancel</button>
+          </footer>
+        </div>
+      </div>
+
+      <h1 className={style.title}>helloworld</h1>
+
+      <button className={style.button} onClick={openModal}>open modal</button>
+    </Layout>
+  )
+}
+
 
 export default IndexPage
