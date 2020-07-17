9cde98bcb227c7c14fef30f09163bc75e675a96c
louiscklaw <louiscklaw@gmail.com>
2020-07-17 15:25:20 +0800

    update to add helloworld script,

--- scripts/new_from_helloworld.sh
+++ scripts/new_from_helloworld.sh
@@ -3,16 +3,4 @@
 set -x
 rm -rf * .*
 
-TEST=`echo $PWD|rev |cut -d'/' -f1 |rev`
-git branch -D test/$TEST
-git checkout -b test/$TEST
-
-set -ex
-
-rsync -avz --exclude 'node_modules' ../gatsby-helloworld/ .
-
-yarn
-
-yarn build
-
-yarn start
+new_from_project gatsby-helloworld
--- scripts/new_from_project.sh
+++ scripts/new_from_project.sh
@@ -7,8 +7,6 @@ TEST=`echo $PWD|rev |cut -d'/' -f1 |rev`
 git branch -D test/$TEST
 git checkout -b test/$TEST
 
-set -ex
-
 rsync -avz --exclude 'node_modules' --exclude 'public' ../$1/ .
 
 yarn
