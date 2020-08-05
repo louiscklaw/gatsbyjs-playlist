5011829c3d40251d6ac7bcee85d6788aeecaf6dc
louiscklaw <louiscklaw@gmail.com>
2020-08-05 16:36:26 +0800

    update and test gh-pages,

--- gatsby-json-hosting-on-github-tryout/src/pages/index.js
+++ gatsby-json-hosting-on-github-tryout/src/pages/index.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby"
 function IndexPage(props){
   return(
     <>
-      <img src="/img/helloworld.png" />
+      <img src="img/helloworld.png" />
       helloworld
     </>
   )
