659fae30cf4a9dfdb869c20018893d1956d34904
louiscklaw <louiscklaw@gmail.com>
2020-08-05 17:14:50 +0800

    upload and test,

--- gatsby-json-hosting-on-github-tryout/src/pages/index.js
+++ gatsby-json-hosting-on-github-tryout/src/pages/index.js
@@ -1,16 +1,38 @@
 import React from "react"
 import { Link } from "gatsby"
 
-function IndexPage(props){
-  React.useEffect(()=>{
+import api from '../api'
 
+const IndexPage = () => {
+  // Create state variables
+  let [ responseData, setResponseData ] = React.useState( '' )
+  React.useEffect(()=>{
+    fetchData()
   },[])
-  return(
-    <>
-      <img src="img/helloworld.png" />
-      helloworld
-    </>
-  )
+
+  // fetches data
+  const fetchData = ( ) => {
+    // e.preventDefault()
+
+    api.getData()
+      .then( ( response ) => {
+        // setResponseData(response.data)
+        console.log( response )
+        setResponseData(response.data)
+      } )
+      .catch( ( error ) => {
+        console.log( error )
+      } )
+  }
+
+
+    return (
+      <div>
+        <pre>
+          {JSON.stringify(responseData, null, 2)}
+        </pre>
+      </div>
+    )
 }
 
 export default IndexPage
