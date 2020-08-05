446ee6c63cfc5bfa3f7dc209d7b2ae15b13bc91e
louiscklaw <louiscklaw@gmail.com>
2020-08-05 16:08:18 +0800

    update example,

--- gatsby-static-folder-tryout/src/pages/index.js
+++ gatsby-static-folder-tryout/src/pages/index.js
@@ -8,18 +8,9 @@ import SEO from "../components/seo"
 import ImgHelloworld from '../components/img_helloworld'
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+  <>
     <img src="/helloworld.png" />
-    <ImgHelloworld />
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </>
 )
 
 export default IndexPage
