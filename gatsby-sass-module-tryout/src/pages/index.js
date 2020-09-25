cc9dd2fbb6c4e876186c0e39e1d5a294f0aead91
louiscklaw <louiscklaw@gmail.com>
2020-09-26 02:40:13 +0800

    update sass module tryout,

--- gatsby-sass-module-tryout/src/pages/index.js
+++ gatsby-sass-module-tryout/src/pages/index.js
@@ -1,17 +1,22 @@
 import React from "react"
+import {Link} from 'gatsby'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import test_style from './styles.module.scss'
+import style from '../scss/styles.module.scss'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <div class={test_style.testClass}>
-      helloworld 123231
-    </div>
-  </Layout>
-)
+function IndexPage() {
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <div class={style.testClass}>
+        helloworld 123231
+      </div>
+      <Link to='/page-1'> to page 1</Link>
+    </Layout>
+    )
+  }
 
 export default IndexPage
