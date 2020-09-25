2bc3d1b2b00e2dd30938c02c754e3af916fa6bab
louiscklaw <louiscklaw@gmail.com>
2020-09-26 03:00:14 +0800

    update to add install script,

---
+++ gatsby-bulmaswatch-tryout/yarn_install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -ex
+
+yarn add @fortawesome/fontawesome-free
+yarn add @fortawesome/fontawesome-svg-core
+yarn add @fortawesome/free-brands-svg-icons
+yarn add @fortawesome/free-regular-svg-icons
+yarn add @fortawesome/free-solid-svg-icons
+yarn add @fortawesome/react-fontawesome
+yarn add bulma
+yarn add gatsby
+yarn add gatsby-image
+yarn add gatsby-plugin-fontawesome-css
+yarn add gatsby-plugin-manifest
+yarn add gatsby-plugin-offline
+yarn add gatsby-plugin-react-helmet
+yarn add gatsby-plugin-sass
+yarn add gatsby-plugin-sharp
+yarn add gatsby-source-filesystem
+yarn add gatsby-transformer-sharp
+yarn add htmltidy
+yarn add node-sass
+yarn add prop-types
+yarn add react
+yarn add react-dom
+yarn add react-helmet
+yarn add react-select
