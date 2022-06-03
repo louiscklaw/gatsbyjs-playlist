eb7ecdd59f01fbe8415de0c635fc728110916ab9
Louis Law <louiscklaw@users.noreply.github.com>
2022-06-04 03:33:36 +0800

    update gatsby-plugin-mdx-tryout,

---
+++ gatsby-plugin-mdx-tryout/gatsby-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-page-creator",
+      options: {
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          posts: require.resolve("./src/components/posts-layout.js"),
+          default: require.resolve("./src/components/default-page-layout.js"),
+        },
+      },
+    },
+  ],
+};
---
+++ gatsby-plugin-mdx-tryout/src/components/default-page-layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { MDXProvider } from "@mdx-js/react";
+import { Link } from "gatsby";
+
+const shortcodes = { Link };
+
+export default ({ children }) => (
+  <MDXProvider components={shortcodes}>
+    default-page-layout{children}
+  </MDXProvider>
+);
